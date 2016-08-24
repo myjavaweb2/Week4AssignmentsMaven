c249d35dd82b840bbb5d0268e9d35637a8e562ad
Kamaldeen Alabi <myjavabootcamp@gmail.com>
2016-08-24 14:24:01 -0400

    Maven Assignment

---
+++ mylabnine/src/main/java/com/mylabnine/WelcomeService.java
@@ -0,0 +1,17 @@
+package com.mylabnine;
+
+import org.eiichiro.gig.Service;
+import org.eiichiro.jaguar.inject.Inject;
+import org.eiichiro.jaguar.inject.Name;
+
+@Name("com.mylabnine.WelcomeService")
+@Service
+public class WelcomeService {
+
+	@Inject private WelcomeRepository repository;
+	
+	public Welcome get() {
+		return repository.get();
+	}
+	
+}
